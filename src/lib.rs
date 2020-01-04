121d3a9208ef0f1553813c0e61bb7d4ad5af3e4d
Aleksa Sarai <cyphar@cyphar.com>
2020-01-05 00:20:14 +1100

    *: #[deny(warnings)] was a mistake
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@
  * with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#![deny(warnings)]
+#![warn(warnings)]
 
 //! libpathrs provides a series of primitives for Linux programs to safely
 //! handle path operations inside an untrusted directory tree.
