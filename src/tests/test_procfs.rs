697fea532f19dd8fbb17bc5abfa0582f47968d28
Aleksa Sarai <cyphar@cyphar.com>
2024-07-30 18:17:46 +1000

    tests: slight rework of test-generation macro
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

--- src/tests/test_procfs.rs
+++ src/tests/test_procfs.rs
@@ -27,7 +27,7 @@ use utils::ExpectedResult;
 
 use anyhow::Error;
 
-macro_rules! procfs_test {
+macro_rules! procfs_tests {
     // Create the actual test functions.
     (@fn [<$func_prefix:ident $test_name:ident>] $procfs_var:ident = { $procfs_inst:expr } <- $do_test:expr => (over_mounts: $over_mounts:expr, error: $expect_error:expr) ;) => {
         paste::paste! {
@@ -70,119 +70,129 @@ macro_rules! procfs_test {
 
     // Create a test for each ProcfsHandle::new_* method.
     (@impl $test_name:ident $procfs_var:ident <- $do_test:expr => ($($tt:tt)*) ;) => {
-        procfs_test! {
+        procfs_tests! {
             @fn [<procfs_overmounts_new_ $test_name>]
                 $procfs_var = { ProcfsHandle::new() } <- $do_test => (over_mounts: false, $($tt)*);
         }
 
-        procfs_test! {
+        procfs_tests! {
             @fn [<procfs_overmounts_new_fsopen_ $test_name>]
                 $procfs_var = { ProcfsHandle::new_fsopen() } <- $do_test => (over_mounts: false, $($tt)*);
         }
 
-        procfs_test! {
+        procfs_tests! {
             @fn [<procfs_overmounts_new_open_tree_ $test_name>]
                 $procfs_var = {
                     ProcfsHandle::new_open_tree(OpenTreeFlags::OPEN_TREE_CLONE)
                 } <- $do_test => (over_mounts: false, $($tt)*);
         }
 
-        procfs_test! {
+        procfs_tests! {
             @fn [<procfs_overmounts_new_open_tree_recursive_ $test_name>]
                 $procfs_var = {
                     ProcfsHandle::new_open_tree(OpenTreeFlags::OPEN_TREE_CLONE | OpenTreeFlags::AT_RECURSIVE)
                 } <- $do_test => (over_mounts: true, $($tt)*);
         }
 
-        procfs_test! {
+        procfs_tests! {
             @fn [<procfs_overmounts_new_unsafe_open $test_name>]
                 $procfs_var = { ProcfsHandle::new_unsafe_open() } <- $do_test => (over_mounts: true, $($tt)*);
         }
     };
 
-    // procfs_test! { abc: readlink("foo") => (error: ExpectedResult::Some(ErrorKind::OsError(Some(libc::ENOENT)))) }
+    // procfs_tests! { abc: readlink("foo") => (error: ExpectedResult::Some(ErrorKind::OsError(Some(libc::ENOENT)))) }
     ($test_name:ident : readlink ( $path:expr ) => ($($tt:tt)*)) => {
         paste::paste! {
-            procfs_test! {
+            procfs_tests! {
                 @impl [<self_readlink_ $test_name>]
                     procfs <- procfs.readlink(ProcfsBase::ProcSelf, $path) => ($($tt)*);
             }
-            procfs_test! {
+            procfs_tests! {
                 @impl [<threadself_readlink_ $test_name>]
                     procfs <- procfs.readlink(ProcfsBase::ProcThreadSelf, $path) => ($($tt)*);
             }
         }
     };
 
-    // procfs_test! { xyz: open("self/fd", O_DIRECTORY) => (error: None) }
+    // procfs_tests! { xyz: open("self/fd", O_DIRECTORY) => (error: None) }
     ($test_name:ident : open ( $path:expr, $($flag:ident)|* ) => ($($tt:tt)*)) => {
         paste::paste! {
-            procfs_test! {
+            procfs_tests! {
                 @impl [<self_open_ $test_name>]
                     procfs <- procfs.open(ProcfsBase::ProcSelf, $path, $(OpenFlags::$flag)|*) => ($($tt)*);
             }
-            procfs_test! {
+            procfs_tests! {
                 @impl [<threadself_open_ $test_name>]
                     procfs <- procfs.open(ProcfsBase::ProcThreadSelf, $path, $(OpenFlags::$flag)|*) => ($($tt)*);
             }
         }
     };
 
-    // procfs_test! { def: open_follow("self/exe", O_DIRECTORY | O_PATH) => (error: ErrorKind::OsError(Some(libc::ENOTDIR) }
+    // procfs_tests! { def: open_follow("self/exe", O_DIRECTORY | O_PATH) => (error: ErrorKind::OsError(Some(libc::ENOTDIR) }
     ($test_name:ident : open_follow ( $path:expr, $($flag:ident)|* ) => ($($tt:tt)*)) => {
         paste::paste! {
-            procfs_test! {
+            procfs_tests! {
                 @impl [<self_open_follow_ $test_name>]
                     procfs <- procfs.open_follow(ProcfsBase::ProcSelf, $path, $(OpenFlags::$flag)|*) => ($($tt)*);
             }
-            procfs_test! {
+            procfs_tests! {
                 @impl [<threadself_open_follow_ $test_name>]
                     procfs <- procfs.open_follow(ProcfsBase::ProcThreadSelf, $path, $(OpenFlags::$flag)|*) => ($($tt)*);
             }
         }
     };
+
+    ($($test_name:ident : $func:ident ($($args:tt)*) => ($($res:tt)*) );* $(;)?) => {
+        paste::paste! {
+            $(
+                procfs_tests!{$test_name : $func ( $($args)* ) => ($($res)*) }
+            )*
+        }
+    }
 }
 
-// Non-procfs overmount.
-procfs_test! { tmpfs_dir: open("fdinfo", O_DIRECTORY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV)))) }
-procfs_test! { tmpfs_dir: open_follow("fdinfo", O_DIRECTORY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV)))) }
-// No overmounts.
-procfs_test! { nomount: open("attr/current", O_RDONLY) => (error: Ok) }
-procfs_test! { nomount: open_follow("attr/current", O_RDONLY) => (error: Ok) }
-// Procfs regular file overmount.
-procfs_test! { proc_file_wr: open("attr/exec", O_WRONLY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV)))) }
-procfs_test! { proc_file_wr: open_follow("attr/exec", O_WRONLY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV)))) }
-procfs_test! { proc_file_rd: open("mountinfo", O_RDONLY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV)))) }
-procfs_test! { proc_file_rd: open_follow("mountinfo", O_RDONLY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV)))) }
-// Magic-links with no overmount.
-procfs_test! { magiclink_nomount: open("cwd", O_PATH) => (error: Ok) }
-procfs_test! { magiclink_nomount: open_follow("cwd", O_RDONLY) => (error: Ok) }
-procfs_test! { magiclink_nomount: readlink("cwd") => (error: Ok) }
-procfs_test! { magiclink_nomount_fd1: readlink("fd/1") => (error: Ok) }
-procfs_test! { magiclink_nomount_fd2: readlink("fd/2") => (error: Ok) }
-// Magic-links with overmount.
-procfs_test! { magiclink_exe: open("exe", O_PATH) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV)))) }
-procfs_test! { magiclink_exe: open_follow("exe", O_RDONLY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV)))) }
-procfs_test! { magiclink_exe: readlink("exe") => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV)))) }
-procfs_test! { magiclink_fd0: open("fd/0", O_PATH) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV)))) }
-procfs_test! { magiclink_fd0: open_follow("fd/0", O_RDONLY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV)))) }
-procfs_test! { magiclink_fd0: readlink("fd/0") => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV)))) }
-// Behaviour-related testing.
-procfs_test! { proc_cwd_trailing_slash: open_follow("cwd/", O_RDONLY) => (error: Ok) }
-procfs_test! { proc_fdlink_trailing_slash: open_follow("fd//1/", O_RDONLY) => (error: Err(ErrorKind::OsError(Some(libc::ENOTDIR)))) }
-// TODO: root can always open procfs files with O_RDWR even if writes fail.
-//procfs_test! { proc_nowrite: open("status", O_RDWR) => (error: Err(ErrorKind::OsError(Some(libc::EACCES)))) }
-procfs_test! { proc_dotdot_escape: open_follow("../..", O_PATH) => (error: Err(ErrorKind::OsError(Some(libc::EXDEV)))) }
-// TODO: openat2(RESOLVE_BENEATH) seems to handle "fd/../.." incorrectly (-EAGAIN), and "fd/.." is allowed.
-//procfs_test! { proc_dotdot_escape: open_follow("fd/../..", O_PATH) => (error: Err(ErrorKind::OsError(Some(libc::EXDEV)))) }
-//procfs_test! { proc_dotdot: open_follow("fd/..", O_PATH) => (error: Err(ErrorKind::OsError(Some(libc::EXDEV)))) }
-procfs_test! { proc_magic_component: open("root/etc/passwd", O_RDONLY) => (error: Err(ErrorKind::OsError(Some(libc::ELOOP)))) }
-procfs_test! { proc_magic_component: open_follow("root/etc/passwd", O_RDONLY) => (error: Err(ErrorKind::OsError(Some(libc::ELOOP)))) }
-procfs_test! { proc_magic_component: readlink("root/etc/passwd") => (error: Err(ErrorKind::OsError(Some(libc::ELOOP)))) }
-procfs_test! { proc_sym_onofollow: open("fd/1", O_RDONLY) => (error: Err(ErrorKind::OsError(Some(libc::ELOOP)))) }
-procfs_test! { proc_sym_opath_onofollow: open("fd/1", O_PATH) => (error: Ok) }
-procfs_test! { proc_sym_odir_opath_onofollow: open("fd/1", O_DIRECTORY|O_PATH) => (error: Err(ErrorKind::OsError(Some(libc::ENOTDIR)))) }
-procfs_test! { proc_dir_odir_opath_onofollow: open("fd", O_DIRECTORY|O_PATH) => (error: Ok) }
+procfs_tests! {
+    // Non-procfs overmount.
+    tmpfs_dir: open("fdinfo", O_DIRECTORY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV))));
+    tmpfs_dir: open_follow("fdinfo", O_DIRECTORY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV))));
+    // No overmounts.
+    nomount: open("attr/current", O_RDONLY) => (error: Ok);
+    nomount: open_follow("attr/current", O_RDONLY) => (error: Ok);
+    // Procfs regular file overmount.
+    proc_file_wr: open("attr/exec", O_WRONLY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV))));
+    proc_file_wr: open_follow("attr/exec", O_WRONLY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV))));
+    proc_file_rd: open("mountinfo", O_RDONLY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV))));
+    proc_file_rd: open_follow("mountinfo", O_RDONLY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV))));
+    // Magic-links with no overmount.
+    magiclink_nomount: open("cwd", O_PATH) => (error: Ok);
+    magiclink_nomount: open_follow("cwd", O_RDONLY) => (error: Ok);
+    magiclink_nomount: readlink("cwd") => (error: Ok);
+    magiclink_nomount_fd1: readlink("fd/1") => (error: Ok);
+    magiclink_nomount_fd2: readlink("fd/2") => (error: Ok);
+    // Magic-links with overmount.
+    magiclink_exe: open("exe", O_PATH) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV))));
+    magiclink_exe: open_follow("exe", O_RDONLY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV))));
+    magiclink_exe: readlink("exe") => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV))));
+    magiclink_fd0: open("fd/0", O_PATH) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV))));
+    magiclink_fd0: open_follow("fd/0", O_RDONLY) => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV))));
+    magiclink_fd0: readlink("fd/0") => (error: ErrOvermount(ErrorKind::OsError(Some(libc::EXDEV))));
+    // Behaviour-related testing.
+    proc_cwd_trailing_slash: open_follow("cwd/", O_RDONLY) => (error: Ok);
+    proc_fdlink_trailing_slash: open_follow("fd//1/", O_RDONLY) => (error: Err(ErrorKind::OsError(Some(libc::ENOTDIR))));
+    // TODO: root can always open procfs files with O_RDWR even if writes fail.
+    // proc_nowrite: open("status", O_RDWR) => (error: Err(ErrorKind::OsError(Some(libc::EACCES))));
+    proc_dotdot_escape: open_follow("../..", O_PATH) => (error: Err(ErrorKind::OsError(Some(libc::EXDEV))));
+    // TODO: openat2(RESOLVE_BENEATH) seems to handle "fd/../.." incorrectly (-EAGAIN), and "fd/.." is allowed.
+    // proc_dotdot_escape: open_follow("fd/../..", O_PATH) => (error: Err(ErrorKind::OsError(Some(libc::EXDEV))));
+    // proc_dotdot: open_follow("fd/..", O_PATH) => (error: Err(ErrorKind::OsError(Some(libc::EXDEV))));
+    proc_magic_component: open("root/etc/passwd", O_RDONLY) => (error: Err(ErrorKind::OsError(Some(libc::ELOOP))));
+    proc_magic_component: open_follow("root/etc/passwd", O_RDONLY) => (error: Err(ErrorKind::OsError(Some(libc::ELOOP))));
+    proc_magic_component: readlink("root/etc/passwd") => (error: Err(ErrorKind::OsError(Some(libc::ELOOP))));
+    proc_sym_onofollow: open("fd/1", O_RDONLY) => (error: Err(ErrorKind::OsError(Some(libc::ELOOP))));
+    proc_sym_opath_onofollow: open("fd/1", O_PATH) => (error: Ok);
+    proc_sym_odir_opath_onofollow: open("fd/1", O_DIRECTORY|O_PATH) => (error: Err(ErrorKind::OsError(Some(libc::ENOTDIR))));
+    proc_dir_odir_opath_onofollow: open("fd", O_DIRECTORY|O_PATH) => (error: Ok);
+}
 
 mod utils {
     use std::{fmt::Debug, path::PathBuf};
