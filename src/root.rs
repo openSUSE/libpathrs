openSUSE/libpathrs

commit 5d3bb1a7e1a27612ac52243fce9ffea367ebdd74

rust api: fix up documentation and #[non_exhaustive]
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

src/root.rs
@@ -46,6 +46,7 @@ use rustix::fs::{self, Dir, SeekFrom};
 ///
 /// [`Root::create`]: struct.Root.html#method.create
 #[derive(Clone, Debug)]
+#[non_exhaustive]
 pub enum InodeType {
     /// Ordinary file, as in [`creat(2)`].
     ///
@@ -159,6 +160,7 @@ impl Root {
     ///
     /// [`Root`]: struct.Root.html
     /// [`Resolver`]: struct.Resolver.html
+    #[doc(alias = "pathrs_root_open")]
     pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
         let file = syscalls::openat(libc::AT_FDCWD, path, libc::O_PATH | libc::O_DIRECTORY, 0)
             .map_err(|err| ErrorImpl::RawOsError {
@@ -263,6 +265,7 @@ impl Root {
     /// [`Handle`]: trait.Handle.html
     /// [`Error`]: error/struct.Error.html
     /// [`Root::resolve_nofollow`]: struct.Root.html#method.resolve_nofollow
+    #[doc(alias = "pathrs_resolve")]
     #[inline]
     pub fn resolve<P: AsRef<Path>>(&self, path: P) -> Result<Handle, Error> {
         self.resolver.resolve(&self.inner, path, false)
@@ -271,8 +274,10 @@ impl Root {
     /// Identical to [`Root::resolve`], except that *trailing* symlinks are
     /// *not* followed and if the trailing component is a symlink
     /// `Root::resolve_nofollow` will return a handle to the symlink itself.
+    /// This is effectively equivalent to `O_NOFOLLOW`.
     ///
     /// [`Root::resolve`]: struct.Root.html#method.resolve
+    #[doc(alias = "pathrs_resolve_nofollow")]
     #[inline]
     pub fn resolve_nofollow<P: AsRef<Path>>(&self, path: P) -> Result<Handle, Error> {
         self.resolver.resolve(&self.inner, path, true)
@@ -290,6 +295,7 @@ impl Root {
     /// [`Root`]: struct.Root.html
     /// [`Root::resolve`]: struct.Root.html#method.resolve
     /// [`Root::resolve_nofollow`]: struct.Root.html#method.resolve_nofollow
+    #[doc(alias = "pathrs_readlink")]
     pub fn readlink<P: AsRef<Path>>(&self, path: P) -> Result<PathBuf, Error> {
         let link = self
             .resolve_nofollow(path)
@@ -312,6 +318,8 @@ impl Root {
     /// inode), an error is returned.
     ///
     /// [`Root`]: struct.Root.html
+    #[doc(alias = "pathrs_creat")]
+    #[doc(alias = "pathrs_create")]
     pub fn create<P: AsRef<Path>>(&self, path: P, inode_type: &InodeType) -> Result<(), Error> {
         // Get a handle for the lexical parent of the target path. It must
         // already exist, and once we have it we're safe from rename races in
@@ -405,6 +413,10 @@ impl Root {
     /// [`Root::create_file`]: struct.Root.html#method.create_file
     /// [`InodeType::File`]: enum.InodeType.html#variant.File
     /// [`O_CREAT`]: http://man7.org/linux/man-pages/man2/open.2.html
+    #[doc(alias = "pathrs_mkdir")]
+    #[doc(alias = "pathrs_mknod")]
+    #[doc(alias = "pathrs_symlink")]
+    #[doc(alias = "pathrs_hardlink")]
     pub fn create_file<P: AsRef<Path>>(
         &self,
         path: P,
@@ -462,11 +474,15 @@ impl Root {
     /// provided path were invalid (non-directory components or dangling symlink
     /// components) or if certain exchange attacks were detected.
     ///
+    /// If an error occurs, it is possible for any number of the directories in
+    /// `path` to have been created despite this method returning an error.
+    ///
     /// [`Root`]: struct.Root.html
     /// [`Handle`]: struct.Handle.html
     /// [`Permissions`]: https://doc.rust-lang.org/stable/std/fs/struct.Permissions.html
     /// [`fs::create_dir_all`]: https://doc.rust-lang.org/stable/std/fs/fn.create_dir_all.html
     /// [`os.MkdirAll`]: https://pkg.go.dev/os#MkdirAll
+    #[doc(alias = "pathrs_mkdir_all")]
     pub fn mkdir_all<P: AsRef<Path>>(&self, path: P, perm: &Permissions) -> Result<Handle, Error> {
         if perm.mode() & !0o7777 != 0 {
             Err(ErrorImpl::InvalidArgument {
@@ -717,6 +733,8 @@ impl Root {
         })?
     }
 
+    // TODO: remove_all()
+
     /// Within the [`Root`]'s tree, perform a rename with the given `source` and
     /// `directory`. The `flags` argument is passed directly to
     /// [`renameat2(2)`].
@@ -727,6 +745,7 @@ impl Root {
     ///
     /// [`Root`]: struct.Root.html
     /// [`renameat2(2)`]: http://man7.org/linux/man-pages/man2/renameat2.2.html
+    #[doc(alias = "pathrs_rename")]
     pub fn rename<P: AsRef<Path>>(
         &self,
         source: P,
@@ -767,8 +786,6 @@ impl Root {
             },
         )
     }
-
-    // TODO: remove_all()
 
     // TODO: implement a way to duplicate (and even serialise) Roots so that you
     //       can send them between processes (presumably with SCM_RIGHTS).

src/error.rs
@@ -41,6 +41,8 @@ use std::{borrow::Cow, error::Error as StdError, io::Error as IOError};
 pub struct Error(#[from] Box<ErrorImpl>);
 
 impl From<ErrorImpl> for Error {
+    // TODO: Is there a way to make this not be exported at all?
+    #[doc(hidden)]
     fn from(err: ErrorImpl) -> Self {
         Self(Box::new(err))
     }

src/handle.rs
@@ -67,6 +67,7 @@ impl Handle {
     ///
     /// [`File`]: https://doc.rust-lang.org/std/fs/struct.File.html
     /// [`Root::create`]: struct.Root.html#method.create
+    #[doc(alias = "pathrs_reopen")]
     pub fn reopen<F: Into<OpenFlags>>(&self, flags: F) -> Result<File, Error> {
         self.inner.reopen(&PROCFS_HANDLE, flags.into())
     }

src/procfs.rs
@@ -37,6 +37,7 @@ use std::{
 // MSRV(1.80): Use LazyLock.
 lazy_static! {
     /// A `procfs` handle to which is used globally by libpathrs.
+    // TODO: Export this?
     pub(crate) static ref PROCFS_HANDLE: ProcfsHandle =
         ProcfsHandle::new().expect("should be able to get some /proc handle");
 }
@@ -57,8 +58,9 @@ lazy_static! {
 /// thread that no longer exists).
 ///
 /// [`ProcfsHandle`]: struct.ProcfsHandle.html
-#[non_exhaustive]
+#[doc(alias = "pathrs_proc_base_t")]
 #[derive(Debug, Clone, Copy)]
+#[non_exhaustive]
 pub enum ProcfsBase {
     /// Use `/proc/self`. For most programs, this is the standard choice.
     ProcSelf,
@@ -338,6 +340,7 @@ impl ProcfsHandle {
     ///
     /// [`ProcfsHandle`]: struct.ProcfsHandle.html
     /// [`ProcfsHandle::open`]: struct.ProcfsHandle.html#method.open
+    #[doc(alias = "pathrs_proc_open")]
     pub fn open_follow<P: AsRef<Path>>(
         &self,
         base: ProcfsBase,
@@ -429,6 +432,7 @@ impl ProcfsHandle {
     /// [`ProcfsBase`]: enum.ProcfsBase.html
     /// [`ProcfsHandle`]: struct.ProcfsHandle.html
     /// [`openat2(2)`]: https://www.man7.org/linux/man-pages/man2/openat2.2.html
+    #[doc(alias = "pathrs_proc_open")]
     pub fn open<P: AsRef<Path>>(
         &self,
         base: ProcfsBase,
@@ -464,6 +468,7 @@ impl ProcfsHandle {
     ///
     /// [`readlinkat(2)`]: https://www.man7.org/linux/man-pages/man2/readlinkat.2.html
     /// [`ProcfsHandle::open`]: struct.ProcfsHandle.html#method.open
+    #[doc(alias = "pathrs_proc_readlink")]
     pub fn readlink<P: AsRef<Path>>(&self, base: ProcfsBase, subpath: P) -> Result<PathBuf, Error> {
         let link = self.open(base, subpath, OpenFlags::O_PATH)?;
         syscalls::readlinkat(link.as_raw_fd(), "").map_err(|err| {

src/resolvers/mod.rs
@@ -50,6 +50,7 @@ const MAX_SYMLINK_TRAVERSALS: usize = 128;
 /// [`Root`]: struct.Root.html
 /// [`Handle`]: struct.Handle.html
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[non_exhaustive]
 pub enum ResolverBackend {
     /// Use the native `openat2(2)` backend (requires kernel support).
     KernelOpenat2,

commit fe1ed4c20b6cf2f0f190f314815744e896621d4d

rust api: fix doc alias brain-o
I got Root::create and Root::create_file confused.

Fixes: 5d3bb1a7e1a2 ("rust api: fix up documentation and #[non_exhaustive]")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

src/root.rs
@@ -326,8 +326,10 @@ impl Root {
     /// inode), an error is returned.
     ///
     /// [`Root`]: struct.Root.html
-    #[doc(alias = "pathrs_creat")]
-    #[doc(alias = "pathrs_create")]
+    #[doc(alias = "pathrs_mkdir")]
+    #[doc(alias = "pathrs_mknod")]
+    #[doc(alias = "pathrs_symlink")]
+    #[doc(alias = "pathrs_hardlink")]
     pub fn create<P: AsRef<Path>>(&self, path: P, inode_type: &InodeType) -> Result<(), Error> {
         // Get a handle for the lexical parent of the target path. It must
         // already exist, and once we have it we're safe from rename races in
@@ -421,10 +423,8 @@ impl Root {
     /// [`Root::create_file`]: struct.Root.html#method.create_file
     /// [`InodeType::File`]: enum.InodeType.html#variant.File
     /// [`O_CREAT`]: http://man7.org/linux/man-pages/man2/open.2.html
-    #[doc(alias = "pathrs_mkdir")]
-    #[doc(alias = "pathrs_mknod")]
-    #[doc(alias = "pathrs_symlink")]
-    #[doc(alias = "pathrs_hardlink")]
+    #[doc(alias = "pathrs_creat")]
+    #[doc(alias = "pathrs_create")]
     pub fn create_file<P: AsRef<Path>>(
         &self,
         path: P,