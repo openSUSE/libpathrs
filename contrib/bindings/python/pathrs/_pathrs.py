openSUSE/libpathrs

commit b951a35a28fa4627d607baa6511e9fa2eb78046c

bindings: add support for PATHRS_PROC_ROOT
For the Go bindings we can just punt on whether we want to expose a raw
ProcOpen() for now, and just create a new wrapper that mirrors
ProcSelfOpen().

Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

contrib/bindings/python/pathrs/_pathrs.py
@@ -220,6 +220,7 @@ def _convert_mode(mode):
 	return flags
 
 
+PROC_ROOT = libpathrs_so.PATHRS_PROC_ROOT
 PROC_SELF = libpathrs_so.PATHRS_PROC_SELF
 PROC_THREAD_SELF = libpathrs_so.PATHRS_PROC_THREAD_SELF
 

go-pathrs/libpathrs_linux.go
@@ -195,6 +195,7 @@ func pathrsHardlink(rootFd uintptr, path, target string) error {
 type pathrsProcBase C.pathrs_proc_base_t
 
 const (
+	pathrsProcRoot       pathrsProcBase = C.PATHRS_PROC_ROOT
 	pathrsProcSelf       pathrsProcBase = C.PATHRS_PROC_SELF
 	pathrsProcThreadSelf pathrsProcBase = C.PATHRS_PROC_THREAD_SELF
 )

go-pathrs/procfs_linux.go
@@ -27,7 +27,10 @@ import (
 type ProcBase int
 
 const (
-	unimplementedProcBaseRoot ProcBase = iota
+	// Use /proc. Note that this mode may be more expensive because we have to
+	// take steps to try to avoid leaking unmasked procfs handles, so you
+	// should use [ProcBaseSelf] if you can.
+	ProcBaseRoot ProcBase = iota
 	// Use /proc/self. For most programs, this is the standard choice.
 	ProcBaseSelf
 	// Use /proc/thread-self. In multi-threaded programs where one thread has
@@ -38,6 +41,8 @@ const (
 
 func (b ProcBase) toPathrsBase() (pathrsProcBase, error) {
 	switch b {
+	case ProcBaseRoot:
+		return pathrsProcRoot, nil
 	case ProcBaseSelf:
 		return pathrsProcSelf, nil
 	case ProcBaseThreadSelf:
@@ -47,24 +52,38 @@ func (b ProcBase) toPathrsBase() (pathrsProcBase, error) {
 	}
 }
 
+func (b ProcBase) namePrefix() string {
+	switch b {
+	case ProcBaseRoot:
+		return "/proc/"
+	case ProcBaseSelf:
+		return "/proc/self/"
+	case ProcBaseThreadSelf:
+		return "/proc/thread-self/"
+	default:
+		return "<invalid procfs base>/"
+	}
+}
+
 // ProcHandleCloser is a callback that needs to be called when you are done
-// operating on an *os.File fetched using ProcThreadSelfOpen.
+// operating on an *os.File fetched using [ProcThreadSelfOpen].
 type ProcHandleCloser func()
 
-// TODO: Consider exporting procOpen once we have ProcBaseRoot.
-
+// TODO: Should we expose procOpen?
 func procOpen(base ProcBase, path string, flags int) (*os.File, ProcHandleCloser, error) {
 	pathrsBase, err := base.toPathrsBase()
 	if err != nil {
 		return nil, nil, err
 	}
+	namePrefix := base.namePrefix()
+
 	switch base {
-	case ProcBaseSelf:
+	case ProcBaseRoot, ProcBaseSelf:
 		fd, err := pathrsProcOpen(pathrsBase, path, flags)
 		if err != nil {
 			return nil, nil, err
 		}
-		return os.NewFile(fd, "/proc/self/"+path), nil, nil
+		return os.NewFile(fd, namePrefix+path), nil, nil
 	case ProcBaseThreadSelf:
 		runtime.LockOSThread()
 		fd, err := pathrsProcOpen(pathrsBase, path, flags)
@@ -72,9 +91,24 @@ func procOpen(base ProcBase, path string, flags int) (*os.File, ProcHandleCloser
 			runtime.UnlockOSThread()
 			return nil, nil, err
 		}
-		return os.NewFile(fd, "/proc/thread-self/"+path), runtime.UnlockOSThread, nil
+		return os.NewFile(fd, namePrefix+path), runtime.UnlockOSThread, nil
 	}
 	panic("unreachable")
+}
+
+// ProcRootOpen safely opens a given path from inside /proc/.
+//
+// This function must only be used for accessing global information from procfs
+// (such as /proc/cpuinfo) or information about other processes (such as
+// /proc/1). Accessing your own process information should be done using
+// [ProcSelfOpen] or [ProcThreadSelfOpen].
+func ProcRootOpen(path string, flags int) (*os.File, error) {
+	file, closer, err := procOpen(ProcBaseRoot, path, flags)
+	if closer != nil {
+		// should not happen
+		panic("non-zero closer returned from procOpen(ProcBaseRoot)")
+	}
+	return file, err
 }
 
 // ProcSelfOpen safely opens a given path from inside /proc/self/.
@@ -86,13 +120,14 @@ func procOpen(base ProcBase, path string, flags int) (*os.File, ProcHandleCloser
 //
 // For such non-heterogeneous processes, /proc/self may reference to a task
 // that has different state from the current goroutine and so it may be
-// preferable to use ProcThreadSelfOpen. The same is true if a user really
+// preferable to use [ProcThreadSelfOpen]. The same is true if a user really
 // wants to inspect the current OS thread's information (such as
 // /proc/thread-self/stack or /proc/thread-self/status which is always uniquely
 // per-thread).
 //
-// Unlike ProcThreadSelfOpen, this method does not involve locking the
-// goroutine to the current OS thread and so is simpler to use.
+// Unlike [ProcThreadSelfOpen], this method does not involve locking the
+// goroutine to the current OS thread and so is simpler to use and
+// theoretically has slightly less overhead.
 func ProcSelfOpen(path string, flags int) (*os.File, error) {
 	file, closer, err := procOpen(ProcBaseSelf, path, flags)
 	if closer != nil {
@@ -105,7 +140,7 @@ func ProcSelfOpen(path string, flags int) (*os.File, error) {
 // ProcThreadSelfOpen safely opens a given path from inside /proc/thread-self/.
 //
 // Most Go processes have heterogeneous threads (all threads have most of the
-// same kernel state such as CLONE_FS) and so ProcSelfOpen is preferable for
+// same kernel state such as CLONE_FS) and so [ProcSelfOpen] is preferable for
 // most users.
 //
 // For non-heterogeneous threads, or users that actually want thread-specific
@@ -129,7 +164,7 @@ func ProcThreadSelfOpen(path string, flags int) (*os.File, ProcHandleCloser, err
 //
 // This is effectively equivalent to doing a Proc*Open(O_PATH|O_NOFOLLOW) of
 // the path and then doing unix.Readlinkat(fd, ""), but with the benefit that
-// thread locking is not necessary for ProcBaseThreadSelf.
+// thread locking is not necessary for [ProcBaseThreadSelf].
 func ProcReadlink(base ProcBase, path string) (string, error) {
 	pathrsBase, err := base.toPathrsBase()
 	if err != nil {

commit 0e1c9dec41e919c03452f95bbb7194768cca69d6

python bindings: add PROC_ROOT to exported list
Fixes: b951a35a28fa ("bindings: add support for PATHRS_PROC_ROOT")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

contrib/bindings/python/pathrs/_pathrs.py
@@ -28,7 +28,7 @@ __all__ = [
 	# core api
 	"Root", "Handle",
 	# procfs api
-	"PROC_SELF", "PROC_THREAD_SELF",
+	"PROC_ROOT", "PROC_SELF", "PROC_THREAD_SELF",
 	"proc_open", "proc_open_raw", "proc_readlink",
 	# error api
 	"Error",