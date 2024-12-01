openSUSE/libpathrs

commit 85d94ab0a7f87476108fb4ded9b876e7b349330e

examples: move all examples to subdirs
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

examples/c/cat.c
@@ -17,7 +17,7 @@
  */
 
 /*
- * File: examples/cat.c
+ * File: examples/c/cat.c
  *
  * An example program which opens a file inside a root and outputs its contents
  * using libpathrs.

examples/c/cat_multithread.c
@@ -17,10 +17,11 @@
  */
 
 /*
- * File: examples/cat_multithread.c
+ * File: examples/c/cat_multithread.c
  *
  * An example program which opens a file inside a root and outputs its contents
- * using libpathrs.
+ * using libpathrs, but multithreaded to show that there are no obvious race
+ * conditions when using pathrs.
  */
 
 #include <stdio.h>

examples/c/.gitignore
@@ -1,3 +1,2 @@
 /cat
 /cat_multithread
-/__pycache__/

examples/python/.gitignore
@@ -1,0 +1,1 @@
+/__pycache__/

examples/python/static_web.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program. If not, see <https://www.gnu.org/licenses/>.
 
-# File: examples/static_web.py
+# File: examples/python/static_web.py
 #
 # An example program which provides a static webserver which will serve files
 # from a directory, safely resolving paths with libpathrs.

commit 158713f4bcaf304b913b638c5f3c60b4434a3775

examples: c: fix includes
In commit 85d94ab0a7f8 ("examples: move all examples to subdirs"), the C
examples were moved to a subdirectory without updating references to
paths outside the examples/ directory.

Fixes: 85d94ab0a7f8 ("examples: move all examples to subdirs")
Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>

examples/c/cat.c
@@ -29,7 +29,7 @@
 #include <string.h>
 #include <errno.h>
 
-#include "../include/pathrs.h"
+#include "../../include/pathrs.h"
 
 #define bail(fmt, ...) \
 	do { fprintf(stderr, fmt "\n", #__VA_ARGS__); exit(1); } while (0)

examples/c/cat_multithread.c
@@ -31,7 +31,7 @@
 #include <errno.h>
 #include <pthread.h>
 
-#include "../include/pathrs.h"
+#include "../../include/pathrs.h"
 
 #define bail(fmt, ...) \
 	do { fprintf(stderr, fmt "\n", #__VA_ARGS__); exit(1); } while (0)